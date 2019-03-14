d50701ed65e2c30fa693813b40f214de9c17f6f9
Jan Jaroš <jan.jaros@o2.cz>
2019-03-14 13:24:35 +0100

    Minesweeper - session: 2019-03-13 - code refactor

--- 20190313-minesweeper/src/main/kotlin/Minesweeper.kt
+++ 20190313-minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,37 +1,26 @@
-fun computeMinesweeperValues(array: IntArray): IntArray {
-    for (index in 0 until array.size) {
-        if (isMine(array[index])) {
-            incrementLeft(array, index)
-            incrementRight(array, index)
+internal class Minesweeper {
+
+    fun computeValues(array: IntArray): IntArray {
+        for (index in 0 until array.size) {
+            if (isMine(array[index])) {
+                incrementLeft(array, index)
+                incrementRight(array, index)
+            }
         }
+        return array
     }
-    return array
-}
 
-fun incrementLeft(array: IntArray, actualIndex: Int) {
-//    if (actualIndex != 0 && !isMine(array[actualIndex - 1]))
-//        ++array[actualIndex - 1]
+    private fun incrementLeft(array: IntArray, actualIndex: Int) {
+        if (actualIndex != 0 && !isMine(array[actualIndex - 1]))
+            ++array[actualIndex - 1]
+    }
 
-    if (!isActualIndexLeftEdge(actualIndex) && !isMine(array[actualIndex - 1]))
-        ++array[actualIndex - 1]
-}
+    private fun incrementRight(array: IntArray, actualIndex: Int) {
+        if (actualIndex < array.size - 1 && !isMine(array[actualIndex + 1]))
+            ++array[actualIndex + 1]
+    }
 
-fun incrementRight(array: IntArray, actualIndex: Int) {
-//    if (actualIndex < array.size - 1 && !isMine(array[actualIndex + 1]))
-//        ++array[actualIndex + 1]
-
-    if (isActualIndexLessThenRightEdge(array, actualIndex) && !isMine(array[actualIndex + 1]))
-        ++array[actualIndex + 1]
-}
-
-fun isMine(value: Int): Boolean {
-    return value == 9
-}
-
-fun isActualIndexLeftEdge(index: Int): Boolean {
-    return index == 0
-}
-
-fun isActualIndexLessThenRightEdge(array: IntArray, index: Int): Boolean {
-    return index < array.size - 1
+    private fun isMine(value: Int): Boolean {
+        return value == 9
+    }
 }
--- 20190313-minesweeper/src/test/kotlin/MinesweeperTest.kt
+++ 20190313-minesweeper/src/test/kotlin/MinesweeperTest.kt
@@ -1,72 +1,40 @@
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 import kotlin.test.assertTrue
-
 
 internal class MinesweeperTest {
 
-    @Test
-    @DisplayName("Test get empty array.")
-    fun testGetEmptyArray() {
-        val inputArray: IntArray = intArrayOf()
-        val expectedArray: IntArray = intArrayOf()
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
+    @DisplayName("Test compute Minesweeper values.")
+    @ParameterizedTest(name = "{index}) input: {0} => expected={1}")
+    @ArgumentsSource(MinesweeperTestArguments::class)
+    fun testComputeMinesweeperValues(inputArray: IntArray, expectedArray: IntArray) {
+        val computedValues = Minesweeper().computeValues(inputArray)
+        assertTrue(expectedArray contentEquals computedValues,
+            "\nExpected values: `${arrayToString(expectedArray)}`\nBut was compute: `${arrayToString(computedValues)}`")
     }
 
-    @Test
-    @DisplayName("Test change minesweeper values.")
-    fun testChangeValues() {
-        val inputArray: IntArray = intArrayOf(0, 9, 0)
-        val expectedArray: IntArray = intArrayOf(1, 9, 1)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
+    class MinesweeperTestArguments : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(intArrayOf(), intArrayOf()),
+                Arguments.of(intArrayOf(0, 9, 0), intArrayOf(1, 9, 1)),
+                Arguments.of(intArrayOf(0, 9, 0, 0), intArrayOf(1, 9, 1, 0)),
+                Arguments.of(intArrayOf(9, 0, 0, 0), intArrayOf(9, 1, 0, 0)),
+                Arguments.of(intArrayOf(0, 0, 0, 9), intArrayOf(0, 0, 1, 9)),
+                Arguments.of(intArrayOf(9, 0, 0, 9), intArrayOf(9, 1, 1, 9)),
+                Arguments.of(intArrayOf(9, 0, 9), intArrayOf(9, 2, 9)),
+                Arguments.of(intArrayOf(9, 0, 9, 0), intArrayOf(9, 2, 9, 1)),
+                Arguments.of(intArrayOf(9, 9, 0), intArrayOf(9, 9, 1))
+            )
+        }
     }
 
-    @Test
-    @DisplayName("Test compute minesweeper values")
-    fun testComputeValues() {
-        val inputArray: IntArray = intArrayOf(0, 9, 0, 0)
-        val expectedArray: IntArray = intArrayOf(1, 9, 1, 0)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
+    private fun arrayToString(array: IntArray): String {
+        return array.joinToString(", ", "[", "]")
     }
-
-    @Test
-    @DisplayName("Test compute left edge minesweeper values")
-    fun testComputeLeftEdgeValues() {
-        val inputArray: IntArray = intArrayOf(9, 0, 0, 0)
-        val expectedArray: IntArray = intArrayOf(9, 1, 0, 0)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
-    }
-
-    @Test
-    @DisplayName("Test compute right edge minesweeper values")
-    fun testComputeRightEdgeValues() {
-        val inputArray: IntArray = intArrayOf(9, 0, 0, 9)
-        val expectedArray: IntArray = intArrayOf(9, 1, 1, 9)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
-    }
-
-    @Test
-    @DisplayName("Test compute middle minesweeper values")
-    fun testComputeMiddleValues() {
-        val inputArray: IntArray = intArrayOf(9, 0, 9)
-        val expectedArray: IntArray = intArrayOf(9, 2, 9)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
-    }
-
-    @Test
-    @DisplayName("Test compute all values")
-    fun testComputeAllValues() {
-        val inputArray: IntArray = intArrayOf(9, 0, 9, 0)
-        val expectedArray: IntArray = intArrayOf(9, 2, 9, 1)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
-    }
-
-    @Test
-    @DisplayName("Test compute all values which two mines are neighbor")
-    fun testComputeTwoMinesNeighbor() {
-        val inputArray: IntArray = intArrayOf(9, 9, 0)
-        val expectedArray: IntArray = intArrayOf(9, 9, 1)
-        assertTrue { expectedArray contentEquals computeMinesweeperValues(inputArray) }
-    }
-
 }
