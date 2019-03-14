4e40e9ff0416978bd2928656f990164b05eaa69c
Jan Jaroš <jan.jaros@o2.cz>
2019-03-14 09:53:33 +0100

    Minesweeper - session: 2019-03-13 (update according to tbartosek's thought)

--- 20190313-minesweeper/src/main/kotlin/Minesweeper.kt
+++ 20190313-minesweeper/src/main/kotlin/Minesweeper.kt
@@ -9,15 +9,29 @@ fun computeMinesweeperValues(array: IntArray): IntArray {
 }
 
 fun incrementLeft(array: IntArray, actualIndex: Int) {
-    if (actualIndex != 0 && !isMine(array[actualIndex - 1]))
+//    if (actualIndex != 0 && !isMine(array[actualIndex - 1]))
+//        ++array[actualIndex - 1]
+
+    if (!isActualIndexLeftEdge(actualIndex) && !isMine(array[actualIndex - 1]))
         ++array[actualIndex - 1]
 }
 
 fun incrementRight(array: IntArray, actualIndex: Int) {
-    if (actualIndex < array.size - 1 && !isMine(array[actualIndex + 1]))
+//    if (actualIndex < array.size - 1 && !isMine(array[actualIndex + 1]))
+//        ++array[actualIndex + 1]
+
+    if (isActualIndexLessThenRightEdge(array, actualIndex) && !isMine(array[actualIndex + 1]))
         ++array[actualIndex + 1]
 }
 
 fun isMine(value: Int): Boolean {
     return value == 9
+}
+
+fun isActualIndexLeftEdge(index: Int): Boolean {
+    return index == 0
+}
+
+fun isActualIndexLessThenRightEdge(array: IntArray, index: Int): Boolean {
+    return index < array.size - 1
 }
